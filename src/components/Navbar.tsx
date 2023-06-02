4011f694f73e048e38919e8195d392afe9cd9eae
e4ewebdev <webdev@e4email.net>
2023-06-02 04:59:24 +0100

    responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 function Navbar() {
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div>
       <nav className="navbar">
@@ -65,15 +71,80 @@ function Navbar() {
                 </Link>
               </div>
             </div>
-            <div className="hidden md:block">
-              <div className="ml-4 flex items-center md:ml-6"></div>
-            </div>
+            <div className="flex md:hidden">
+            <button
+              onClick={toggleNavbar}
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-[#31240B] focus:outline-none focus:bg-[#31240B] focus:text-white"
+            >
+              <svg className={`${isOpen ? 'hidden' : 'block'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+              <svg className={`${isOpen ? 'block' : 'hidden'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
           </div>
         </div>
-        <div className="md:hidden">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3"></div>
-        </div>
+        <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`}>
+        <div className="block">
+          <ul className="text-center pt-[2rem]">
+            <li className="mb-2">
+            <Link
+                  to="#"
+                  className=" px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Careers
+                </Link>
+            </li>
+
+            <li className="mb-2">
+            <Link
+                  to="#"
+                  className=" px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  FAQs
+                </Link>
+            </li>
+
+            <li className="mb-2">
+            <Link
+                  to="#"
+                  className=" px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Testimonials
+                </Link>
+            </li>
+
+            <li className="mb-8">
+            <Link
+                  to="#"
+                  className=" px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  About Us
+                </Link>
+            </li>
+          </ul>      
+                <div className="ml-10 block items-baseline space-x-4">
+                <button
+                  className="rounded-full base-btn hover:bg-[#31240B]"
+                  style={{maxWidth: '12.188rem', height: '3.5rem'}}
+                  
+                >
+                  Send Money
+                </button>
+                <Link
+                  to="/login"
+                  className=" px-3 py-2 rounded-md text-sm font-medium"
+                  style={{marginLeft: '20px'}}
+                >
+                  Login
+                </Link>
+              </div>
+         </div>
+    </div>
       </nav>
+
     </div>
   );
 }
